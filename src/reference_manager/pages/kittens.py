c557443ec28b09b360b01023a5e964389dda0b6f
dmitrylala <dim.nesterov2015@gmail.com>
2022-06-13 01:27:47 +0300

    added random cat gif

--- src/reference_manager/pages/kittens.py
+++ src/reference_manager/pages/kittens.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 HEADER = "Котики"
 MORE_KITTENS = "Хочу котика!"
+MORE_KITTENS_GIF = "Хочу гифку с котиком!"
 CAT_API = "https://api.thecatapi.com/v1/images/search"
 TOKEN = st.secrets["cat_api_token"]
 
@@ -13,13 +14,26 @@ def get_random_cat():
     return response.json()[0]["url"]
 
 
+def get_random_cat_gif():
+    while True:
+        response = requests.get(CAT_API, params={"x-api-key": TOKEN})
+        url = response.json()[0]["url"]
+        if url.endswith(".gif"):
+            return url
+
+
 def main():
     st.markdown(f"# {HEADER} 😺")
     st.sidebar.markdown(f"# {HEADER} 😺️")
 
-    if st.button(MORE_KITTENS):
-        url = get_random_cat()
-        st.markdown(f"![Alt Text]({url})")
+    more_kittens = st.button(MORE_KITTENS)
+    more_kittens_gif = st.button(MORE_KITTENS_GIF)
+
+    if more_kittens:
+        st.markdown(f"![Alt Text]({get_random_cat()})")
+
+    if more_kittens_gif:
+        st.markdown(f"![Alt Text]({get_random_cat_gif()})")
 
 
 if __name__ == "__main__":
