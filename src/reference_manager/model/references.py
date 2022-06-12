bfc9cd8b13417841b70950ad77129c9d1bd63e26
dmitrylala <dim.nesterov2015@gmail.com>
2022-06-12 13:11:41 +0300

    monography output updates

--- src/reference_manager/model/references.py
+++ src/reference_manager/model/references.py
@@ -81,13 +81,13 @@ class Monography(Reference):
         editor = f"{self.editor}" if self.editor else ""
         semicolon = "; " if editor else ""
         translator = f"{semicolon}пер. {self.translator}" if self.translator else ""
-        backslashes = "// " if editor or translator else ""
+        backslashes = " // " if editor or translator else ""
 
-        res_transtextual = f"{self.author} ({self.year}) {self.name} " \
+        res_transtextual = f"{self.author} ({self.year}) {self.name}" \
                            f"{backslashes}{editor}{translator}. — " \
                            f"{self.city}: {self.publishing_house}. " \
                            f"— С. {self.pages}."
-        res_subscript = f"{self.author} {self.name} {backslashes}" \
+        res_subscript = f"{self.author} {self.name}{backslashes}" \
                         f"{editor}{translator}. — " \
                         f"{self.city}: {self.publishing_house}, " \
                         f"{self.year}. — С. {self.pages}."
